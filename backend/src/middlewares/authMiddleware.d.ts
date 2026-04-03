ec50cef68b29486e055c69f33ab8c9d1a140f30a
NyaMariah <rasoanaivonyainamariah@gmail.com>
2026-04-04 00:26:08 +0300

    supp

--- backend/src/middlewares/authMiddleware.d.ts
+++
@@ -1,3 +0,0 @@
-import { Response, NextFunction } from "express";
-export declare const authenticateToken: (req: any, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
-//# sourceMappingURL=authMiddleware.d.ts.map
